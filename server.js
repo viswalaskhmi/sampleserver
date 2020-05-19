3efc5d15341ce1abeaeb7b97f4f6787c99f81f24
Viswalakshmi <viswalakshmi_ece@nec.edu.in>
2020-05-19 19:08:55 +0530

    added two functionalities to demonstrate json

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require('express');
+var file = require('fs');
 var app =express();
+var bodyparser = require('body-parser')
+app.use(bodyparser.json())
 var server = app.listen(8085,function()
 {
     var host = server.address().host
@@ -16,5 +19,22 @@ app.get("/checkuser/:name/:age",function(req,res)
  else
    out = "Hi "+name+", Sorry - please wait to reach 18"
    res.end(JSON.stringify(out));
+   
+   
 })
+app.get("/voterslist",function(req,res)
+{
+  const voterslist= file.readFileSync('voterslist.json')
+  console.log(JSON.parse(voterslist));
+  res.end(voterslist)
+
+})
+app.post("/addnew",function(req,res)
+{
+  console.log(req.body)
+  const newvoter = JSON.stringify(req.body)
+  file.appendFileSync('voterslist.json',newvoter)
+  res.end("Voter id added successfully")
+}
+)
 
